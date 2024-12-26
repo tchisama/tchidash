1394388f027c53835d9fda9ad320a9f0459adf38
tchisama <pro.tchisama@gmail.com>
2024-12-26 15:46:22 +0100

    changing the font

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import AuthProvider from "./AuthProvider";
 import Providers from "./Providers";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "./dashboard/components/ThemeProvider";
+import {Outfit} from "next/font/google";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -17,6 +18,13 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+const OutfitFont = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-geist-sans",
+  display: "swap",
+})
 
 export const metadata: Metadata = {
   title: "Tchidash",
@@ -31,7 +39,7 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang="en">
-        <body className={`light text-black/80 ${geistSans.variable} ${geistMono.variable}`}>
+        <body className={`light ${OutfitFont.className} text-black/80 ${geistSans.variable} ${geistMono.variable}`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="light"
